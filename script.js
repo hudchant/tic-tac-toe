a63fe0f5ec6382e4b1aca45127ddb4c88f97947b
hudchant <hudchant@gmail.com>
2026-03-03 19:39:11 -0800

    Create factory functions to control gameplay

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ const theGame = (() => {
 
     function gameBoard() {
         const board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
+        // Object containing board array
         return { board };
     }
-    return gameBoard();
 
+    function players() {
+        const gamePlayers = ['Player One', 'Player Two'];
+        // Object containing gamePlayers array
+        return { gamePlayers };
+    }
+
+    // Destructure the function return values
+    const { board } = gameBoard();
+    const { gamePlayers } = players();
+
+    // Return object that includes the game board and the game players
+    return { board, gamePlayers };
 })();
 
 console.log(theGame.board);
+console.log(theGame.gamePlayers);
