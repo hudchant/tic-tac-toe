0d24f5eeda7352943a502ed46ced18ddfb07611d
hudchant <hudchant@gmail.com>
2026-03-03 17:30:04 -0800

    Move gameboard from html file into the script file

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
 // Gameboard objects
-function gameBoard() {
+const theGame = (() => {
 
-    document.addEventListener('DOMContentLoaded', function () {
-        const allBoxes = document.querySelectorAll('.boxes');
-        const board = [];
-
-        allBoxes.forEach(box => {
-            let currentPlayer;
-            let firstPlayer = 'X';
-            let secondPlayer = 'O';
-            const [first, second, third, fourth, fifth, sixth, seventh, eighth, ninth] = board;
-
-            box.addEventListener('click', () => {
-                for (let i = 1; i < 10; i++) {
-                    if (i % 2 !== 0) {
-                        box.textContent += firstPlayer;
-                    } else {
-                        box.textContent += secondPlayer;
-                    }
-                }
-            });
-        });
-
+    function gameBoard() {
+        const board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
         return { board };
-    });
-}
+    }
+    return gameBoard();
+
+})();
+
+console.log(theGame.board);
